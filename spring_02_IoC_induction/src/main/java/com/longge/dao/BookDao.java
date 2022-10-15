7bb7e28cfe560e3f23cef8570e465c7fd4c94f32
williamlongwong <william.long.wong@gmail.com>
2022-10-15 09:58:27 +0800

    Spring仓库初始化操作

---
+++ spring_02_IoC_induction/src/main/java/com/longge/dao/BookDao.java
@@ -0,0 +1,5 @@
+package com.longge.dao;
+
+public interface BookDao {
+    public void save();
+}
---
+++ spring_02_IoC_induction/src/main/java/com/longge/service/BookService.java
@@ -0,0 +1,5 @@
+package com.longge.service;
+
+public interface BookService {
+    public void save();
+}
